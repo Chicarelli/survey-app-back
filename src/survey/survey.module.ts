2621d556b4a8274c5d9c264986acddcf0656b754
Rafael Chicarelli Lopes <rafaelsep20@gmail.com>
2022-11-20 18:27:26 -0300

    feat: update survey modules

--- src/survey/survey.module.ts
+++ src/survey/survey.module.ts
@@ -1,9 +1,31 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { SurveyService } from './survey.service';
 import { SurveyController } from './survey.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
+import { Survey, SurveySchema } from './schemas/survey.schema';
+import { UsersService } from 'src/users/users.service';
+import { AuthValidateMiddleware } from 'src/middleware/authValidate.middleware';
 
 @Module({
   controllers: [SurveyController],
-  providers: [SurveyService]
+  providers: [SurveyService, UsersService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Survey.name, schema: SurveySchema },
+    ]),
+  ],
 })
-export class SurveyModule {}
+export class SurveyModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthValidateMiddleware)
+      .forRoutes({ path: 'survey', method: RequestMethod.POST });
+  }
+}
