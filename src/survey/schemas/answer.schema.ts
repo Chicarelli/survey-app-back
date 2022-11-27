e65f39c40f60a7028e81427dbd783390580982d2
Rafael Chicarelli Lopes <rafaelsep20@gmail.com>
2022-11-27 15:56:23 -0300

    feat: add answer schema

---
+++ src/survey/schemas/answer.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { User } from 'src/users/schemas/user.schema';
+import { Survey } from './survey.schema';
+
+export type AnswerDocument = HydratedDocument<Answer>;
+
+@Schema()
+export class Answer {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'survey' })
+  survey: Survey;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'user' })
+  owner: User;
+
+  @Prop()
+  connectionId: string;
+
+  @Prop()
+  answer: Answer[];
+}
+
+export interface Answer {
+  question_key: number;
+  option_key: number;
+  option_text: string;
+}
+
+export const AnswerSchema = SchemaFactory.createForClass(Answer);
