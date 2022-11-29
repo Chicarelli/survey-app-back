210c48f94ab652440b66d1b0f4a71ac79ea06fae
Rafael Chicarelli Lopes <rafaelsep20@gmail.com>
2022-11-28 21:34:57 -0300

    chore: refactor code. Putting business logic at domain

--- src/survey/schemas/survey.schema.ts
+++ src/survey/schemas/survey.schema.ts
@@ -43,11 +43,80 @@ export class Survey {
     this.owner = owner;
     this.isPublic = isPublic;
     this.isUniqueAnswer = isUniqueAnswer;
-    this.title = title;
     this.description = description;
-    this.finalDate = finalDate;
     this.limitAnswer = limitAnswer;
+    this.setTitle(title);
+    this.setQuestions(questions);
+    this.setFinalDate(finalDate);
+  }
+
+  setQuestions(questions: Question[]) {
+    this.isQuestionsValid(questions);
+
     this.questions = questions;
+  }
+
+  private isQuestionsValid(questions: Question[]): void {
+    console.log(`Checking if it is a valid survey`);
+    this.checkIfThereIsMinimumQuestions(questions.length);
+    this.checkEmptyQuestions(questions);
+    this.checkQuestionsAnswers(questions);
+  }
+
+  private checkIfThereIsMinimumQuestions(
+    questions: number | Array<Question>,
+  ): void {
+    console.log(`Checking if there is a Minimum question`);
+    const MINIMUM_QUESTION = 2;
+    let numberOfQuestions: number;
+
+    if (typeof questions === 'object') {
+      numberOfQuestions = questions.length;
+    } else {
+      numberOfQuestions = Number(questions);
+    }
+
+    if (numberOfQuestions < MINIMUM_QUESTION)
+      throw new Error('Pesquisa tem que ter pelo menos duas questões');
+  }
+
+  private checkEmptyQuestions(questions: Array<Question>): void {
+    console.log(`Checking if there is some empty question`);
+    questions.forEach((question) => {
+      if (
+        !question.question ||
+        question.question.replace(/\s/g, '').length == 0
+      ) {
+        throw new Error('Questões vazias não são permitidas');
+      }
+    });
+  }
+
+  private checkQuestionsAnswers(questions: Array<Question>): void {
+    console.log(`Cheking questions answers`);
+    questions.forEach((question) => {
+      if (question.answers.length < 2) {
+        throw new Error('Questões precisam ter pelo menos duas alternativas');
+      }
+    });
+  }
+
+  public setFinalDate(finalDate): void {
+    const date = new Date(finalDate);
+
+    if (isNaN(Number(date)) || !finalDate) {
+      this.finalDate = null;
+    }
+
+    this.finalDate = date.toString();
+  }
+
+  public setTitle(title: string) {
+    if (title.replace(/\s/g, '').length === 0) {
+      throw new Error('Nome inválido');
+    }
+
+    this.title = title;
   }
 }
 
--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -18,16 +18,12 @@ export class SurveyService {
     createSurveyDto: CreateSurveyDto,
     user: User,
   ): Promise<Survey> {
-    this.isValidSurvey(createSurveyDto);
-
     console.log(`Creating survey ${createSurveyDto.title} from ${user.email}`);
     const createdSurvey = new this.surveyModel({
       owner: user,
       ...createSurveyDto,
-      finalDate: this.treatSurveyFinalDate(createSurveyDto.finalDate),
     });
 
-    console.log(`Saving survey`);
     await createdSurvey.save();
 
     return createdSurvey;
@@ -57,69 +53,6 @@ export class SurveyService {
     await answer.save();
 
     return answer;
-  }
-
-  private isValidSurvey(survey: CreateSurveyDto): void {
-    this.isQuestionsValid(survey.questions);
-
-    if (survey.title.replace(/\s/g, '').length === 0) {
-      throw new Error('Nome inválido');
-    }
-  }
-
-  private isQuestionsValid(questions: Question[]): void {
-    console.log(`Checking if it is a valid survey`);
-    this.checkIfThereIsMinimumQuestions(questions.length);
-    this.checkEmptyQuestions(questions);
-    this.checkQuestionsAnswers(questions);
-  }
-
-  private checkIfThereIsMinimumQuestions(
-    questions: number | Array<Question>,
-  ): void {
-    console.log(`Checking if there is a Minimum question`);
-    const MINIMUM_QUESTION = 2;
-    let numberOfQuestions: number;
-
-    if (typeof questions === 'object') {
-      numberOfQuestions = questions.length;
-    } else {
-      numberOfQuestions = Number(questions);
-    }
-
-    if (numberOfQuestions < MINIMUM_QUESTION)
-      throw new Error('Pesquisa tem que ter pelo menos duas questões');
-  }
-
-  private checkEmptyQuestions(questions: Array<Question>): void {
-    console.log(`Checking if there is some empty question`);
-    questions.forEach((question) => {
-      if (
-        !question.question ||
-        question.question.replace(/\s/g, '').length == 0
-      ) {
-        throw new Error('Questões vazias não são permitidas');
-      }
-    });
-  }
-
-  private checkQuestionsAnswers(questions: Array<Question>): void {
-    console.log(`Cheking questions answers`);
-    questions.forEach((question) => {
-      if (question.answers.length < 2) {
-        throw new Error('Questões precisam ter pelo menos duas alternativas');
-      }
-    });
-  }
-
-  private treatSurveyFinalDate(finalDate: string): string {
-    const date = new Date(finalDate);
-
-    if (isNaN(Number(date)) || !finalDate) {
-      return '';
-    }
-
-    return date.toString();
   }
 
   private checkAnswers(survey: Survey, answers: AnswerProp[]): void {
